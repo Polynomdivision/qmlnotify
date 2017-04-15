f6b24bac859778f116efbb02e577f9d334c548d6
Alexander Polynomdivision <digitalmail555@googlemail.com>
2017-04-15 18:43:07 +0000

    Implement a global and a user directory for storing qml components

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
+#include <cstdlib>
+#include <array>
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDebug>
+#include <QFile>
+
 #include "notification/notificationmanager.h"
+
+const QString home_path = QString(std::getenv("HOME"));
+const std::array<QString, 2> search_paths = {
+    home_path + QString("/.config/qmlnotify/"), // Home directory
+    QString("/etc/qmlnotify/") // Global configuration directory
+};
+
+// Return the path of the component.
+// Returns "" if the component is not in the search paths.
+QString get_component_path(QString component) {
+    for (unsigned int i = 0; i < search_paths.size(); i++) {
+        // qDebug() << "Checking" << search_paths[i];
+        // qDebug() << search_paths[i] + component;
+	if (QFile::exists(search_paths[i] + component)) {
+	    return search_paths[i] + component;
+	}
+    }
+
+    // No match found
+    return QString("");
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,8 +41,14 @@ int main(int argc, char *argv[])
     for (int i = 0; i < argc; i++) {
         if (QString(argv[i]) == "--qml") {
             if (++i < argc) {
-                component_path = QString(argv[i]);
-            }
+                component_path = get_component_path(QString(argv[i]).trimmed());
+		
+		// Tell the user that we were not able to find the component
+		if (component_path == "") {
+           	    qDebug() << "Error: Could not find" << QString(argv[i]).trimmed() << "in the search paths";
+		    return 1;
+		} 
+	    }
         }
     }
 
